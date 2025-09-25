5095cc9aaa0b25beb5a2a20cb0c3512a40ecaa35
HenriqueGMen <henrimeen24@gmail.com>
2025-09-24 21:39:59 -0300

    Resolução Exercício2

---
+++ Capitulo3/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            int cod1, cod2, qtd1, qtd2;
+            double preco1, preco2, total;
+
+            Console.WriteLine("Digite as informações do primeiro produto");
+
+            string[] info = Console.ReadLine().Split(' ');
+
+            cod1 = int.Parse(info[0]);
+            qtd1 = int.Parse(info[1]);
+            preco1 = double.Parse(info[2], CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Digite as informações do segundo produto");
+
+            info = Console.ReadLine().Split(' ');
+
+            cod2 = int.Parse(info[0]);
+            qtd2 = int.Parse(info[1]);
+            preco2 = double.Parse(info[2], CultureInfo.InvariantCulture);
+
+
+            total = (qtd1 * preco1) + (qtd2 * preco2);
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
+
