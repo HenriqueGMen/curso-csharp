98073527614d8d930f2aab003326afc635d79184
HenriqueGMen <henrimeen24@gmail.com>
2025-10-09 14:31:36 -0300

    FEAT: calculo salario POO

---
+++ Capitulo4/Exercicio4/Exercicio4/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Globalization;
+
+namespace Exercicio4
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"Funcionário: {Nome}\n" +
+            $"Salário Líquido: {SalarioLiquido().ToString("C", new CultureInfo("pt-BR"))}";
+        }
+    }
+}
---
+++ Capitulo4/Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + funcionario);
+
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salário em qual porcentagem? (SIM, NÃO)");
+            string confimacao = Console.ReadLine().ToLower();
+
+            if (confimacao.Equals("sim"))
+            {
+                Console.Write("Digite o valor do aumento: ");
+                double aumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                funcionario.AumentarSalario(aumento);
+
+                Console.WriteLine();
+                Console.WriteLine("Dados atualizados: " + funcionario);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para fechar o programa...");
+            Console.ReadKey();
+        }
+
+    }
+}
