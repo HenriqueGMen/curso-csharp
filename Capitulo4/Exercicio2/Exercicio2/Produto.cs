e1559ea54393693eb176ed452aa1bf347bac6b03
HenriqueGMen <henrimeen24@gmail.com>
2025-10-08 22:13:54 -0300

    Exercicio2 Cap4

---
+++ Capitulo4/Exercicio2/Exercicio2/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cap4Exercico2
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEstoque()
+        {
+            return Quantidade * Preco;
+        }
+
+        public override string ToString()
+        {
+           return $"Produto: {Nome}\n" +
+           $"Preço: {Preco.ToString("C", new CultureInfo("pt-BR"))}\n" +
+           $"Quantidade: {Quantidade} unidades\n" +
+           $"Preço Total: {ValorTotalEstoque().ToString("C", new CultureInfo("pt-BR"))}";
+        }
+    }
+}
---
+++ Capitulo4/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Cap4Exercico2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), new CultureInfo("pt-BR"));
+
+            Console.Write("Quantidade: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do Produto: " + produto);
+
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+
+        }
+    }
+}
