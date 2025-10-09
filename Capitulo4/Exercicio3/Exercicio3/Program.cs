8cae6b71abed45dfd2e5be385cef2e14230047da
HenriqueGMen <henrimeen24@gmail.com>
2025-10-09 09:00:24 -0300

    FEAT: adiciona métodos de calculo Retangulo

---
+++ Capitulo4/Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+
+            Console.Write("Digite a Largura do retangulo: ");
+            ret.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Digite a Altura do retangulo: ");
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Resultados:");
+            Console.WriteLine($"Área: {ret.Area().ToString("F2", CultureInfo.InvariantCulture)}" );
+            Console.WriteLine($"Perímetro: {ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}" );
+            Console.WriteLine($"Diagonal: {ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}" );
+
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
