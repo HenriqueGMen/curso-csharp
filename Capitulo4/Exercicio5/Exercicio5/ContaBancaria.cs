273840d01a4ea2d3b9e2c233fb63a9da78d19785
HenriqueGMen <henrimeen24@gmail.com>
2025-10-16 23:50:58 -0300

    Resolução cap4 exercicio6

--- Capitulo4/Exercicio5/Exercicio5/ContaBancaria.cs
+++ Capitulo4/Exercicio5/Exercicio5/ContaBancaria.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Globalization;
 
-namespace Exercico5
+namespace Exercicio5
 {
     class ContaBancaria
     {
@@ -8,6 +9,48 @@ namespace Exercico5
         public string Titular { get; set; }
         public double Saldo { get; private set; }
 
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
 
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public bool Saque(double quantia)
+        {
+            if (quantia <= 0)
+            {
+                Console.WriteLine("O valor do saque deve ser positivo!");
+                return false;
+            }
+
+            if (quantia > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente para realizar o saque!");
+                return false;
+            }
+
+            Saldo -= quantia;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("C", new CultureInfo("pt-BR"));
+        }
     }
 }
--- Capitulo4/Exercicio5/Exercicio5/Program.cs
+++ Capitulo4/Exercicio5/Exercicio5/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Digite o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do titula da conta:");
+            string titular = Console.ReadLine();
+
+            Console.Write("Deseja fazer um depósito inicial (s/n)? ");
+            string resp = Console.ReadLine().ToLower();
+
+            if(resp == "s")
+            {
+                Console.Write("Informe o valor do depósito: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para fechar o programa...");
+            Console.ReadKey();
+        }
+    }
+}
