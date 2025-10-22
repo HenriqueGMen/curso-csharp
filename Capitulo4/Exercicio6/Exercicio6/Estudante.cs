649d70da4e7198e28367e54e00ac4d840045d830
HenriqueGMen <henrimeen24@gmail.com>
2025-10-21 22:07:27 -0300

    Exercicio 6 POO

--- Capitulo4/Exercicio6/Exercicio6/Estudante.cs
+++ Capitulo4/Exercicio6/Exercicio6/Estudante.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Exercicio6
 {
-    internal class Estudante
+    class Estudante
     {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + " - " + Email;
+        }
     }
 }
--- Capitulo4/Exercicio6/Exercicio6/Program.cs
+++ Capitulo4/Exercicio6/Exercicio6/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] estudantes = new Estudante[10];
+
+            Console.WriteLine("Quantos quartos serão alugados? ");
+            int quartosAlugados = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < quartosAlugados; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel ● {i}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                while (estudantes[quarto] != null)
+                {
+                    Console.WriteLine("❌ Esse quarto já está ocupado! Escolha outro.");
+                    quarto = int.Parse(Console.ReadLine());
+                }
+
+                estudantes[quarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Relatório de ocupação");
+
+            for(int i = 0; i < 10; i++)
+            {
+                if (estudantes[i] != null)
+                {
+                    Console.WriteLine(i + ": " + estudantes[i]);
+                }
+            }
+        }
+    }
+}
